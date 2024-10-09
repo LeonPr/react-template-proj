f8569dd00e7cbe6931fb71a17c2fb4d7eb9d63ec
LeonPr <leon@masig.co.il>
2024-10-09 17:42:02 +0300

    Add header

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,13 +1,26 @@
 import { Home } from "./pages/Home.jsx"
+import { OnLineBooks } from "./pages/BookIndex.jsx"
+import { About } from "./pages/AboutUs.jsx"
+import { AppHeader } from "./cmps/AppHeader.jsx"
+
+const { useState } = React
 
 export function App() {
+    const [page, setPage] = useState('books')
+
     return (
         <section className="app">
-            <header className="app-header main-layout">
+            <AppHeader onSetPage={(page) => setPage(page)} />
+            {/* <header className="app-header main-layout">
                 <h1>Miss Books</h1>
-            </header>
+            </header> */}
             <main className="main-layout">
-                <Home />
+
+                {page === 'home' && <Home />}
+                {page === 'about' && <About />}
+                {page === 'books' && <OnLineBooks />}
+
+                {/* <Home /> */}
             </main>
         </section>
     )
